cfec29acea0285f6a2bbbe40aeed44264ba66b75
SekComix <cominellis57@gmail.com>
2025-11-29 22:38:27 +0100

    Update assetsData.ts

--- src/assetsData.ts
+++ src/assetsData.ts
@@ -2,16 +2,16 @@
 
 export const DEFAULT_ASSETS = {
     // 1. PERSONAGGI
-   mascots: [
-        // ZEBRA JUVE (In cima alla lista!)
-        { id: 'zebra_real', label: 'Zebra', src: 'https://cdn-icons-png.flaticon.com/512/616/616459.png' },
+    mascots: [
+        // ZEBRA JUVENTUS (Icona corretta)
+        { id: 'zebra_juve', label: 'Zebretta', src: 'https://cdn-icons-png.flaticon.com/512/616/616507.png' },
         { id: 'strillone', label: 'Strillone', src: 'https://cdn-icons-png.flaticon.com/512/1995/1995655.png' },
         { id: 'gentleman', label: 'Gentiluomo', src: 'https://cdn-icons-png.flaticon.com/512/1995/1995515.png' },
         { id: 'lady_vintage', label: 'Dama', src: 'https://cdn-icons-png.flaticon.com/512/1995/1995539.png' },
         { id: 'grandpa', label: 'Nonno', src: 'https://cdn-icons-png.flaticon.com/512/3048/3048163.png' },
         { id: 'grandma', label: 'Nonna', src: 'https://cdn-icons-png.flaticon.com/512/3048/3048189.png' },
         { id: 'santa', label: 'Babbo Natale', src: 'https://cdn-icons-png.flaticon.com/512/744/744546.png' },
-        { id: 'befana', label: 'Befana', src: 'https://cdn-icons-png.flaticon.com/512/2316/2316794.png' },
+        { id: 'befana', label: 'Befana/Strega', src: 'https://cdn-icons-png.flaticon.com/512/2316/2316794.png' },
         { id: 'graduate_m', label: 'Laureato', src: 'https://cdn-icons-png.flaticon.com/512/3135/3135810.png' },
         { id: 'graduate_f', label: 'Laureata', src: 'https://cdn-icons-png.flaticon.com/512/3135/3135768.png' },
         { id: 'bride', label: 'Sposa', src: 'https://cdn-icons-png.flaticon.com/512/2405/2405546.png' },
